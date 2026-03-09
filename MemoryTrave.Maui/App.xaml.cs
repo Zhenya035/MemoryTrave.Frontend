90e91b1416a7533a26a6ee604d9fac6b52f2d195
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-03-09 21:40:58 +0300

    up: add check token #2

--- MemoryTrave.Maui/App.xaml.cs
+++ MemoryTrave.Maui/App.xaml.cs
@@ -1,17 +1,33 @@
-﻿namespace MemoryTrave.Maui;
+﻿using MemoryTrave.Maui.Infrastructure.Api;
+using MemoryTrave.Maui.Services.Interfaces;
+
+namespace MemoryTrave.Maui;
 
 public partial class App : Application
 {
     private readonly IServiceProvider _services;
-    public App(IServiceProvider services)
+    private readonly ApiRequestService _apiService;
+    private readonly IStorageService _storageService;
+
+    public App(IServiceProvider services, ApiRequestService apiService, IStorageService storageService)
     {
         InitializeComponent();
         _services = services;
+        _apiService = apiService;
+        _storageService = storageService;
     }
 
     protected override Window CreateWindow(IActivationState? activationState)
     {
+        CheckToken();
         var appShell = _services.GetRequiredService<View.AppShell>();
         return new Window(appShell);
+    }
+
+    private async void CheckToken()
+    {
+        var token = await _storageService.GetTokenAsync();
+        if (!string.IsNullOrWhiteSpace(token))
+            _apiService.SetJwtToken(token);
     }
 }
