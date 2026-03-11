617d47e7ad0bc19d0df32bee549e792b3aafdfc0
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-03-11 17:57:29 +0300

    up: add map base #2

--- MemoryTrave.Maui/View/MapPage.xaml.cs
+++ MemoryTrave.Maui/View/MapPage.xaml.cs
@@ -1,12 +1,47 @@
-﻿using MemoryTrave.Maui.ViewModel;
+﻿using Mapsui;
+using Mapsui.Layers;
+using Mapsui.UI.Maui;
+using MemoryTrave.Maui.ViewModel;
+using MemoryTrave.Maui.Models.Location;
+using Location = MemoryTrave.Maui.Models.Location.Location;
 
 namespace MemoryTrave.Maui.View;
 
 public partial class MapPage : ContentPage
 {
+    private readonly MapViewModel _viewModel;
+
     public MapPage(MapViewModel viewModel)
     {
         InitializeComponent();
-        BindingContext = viewModel;
+        BindingContext = _viewModel = viewModel;
+
+        MapControl.Info += OnMapInfo;
+    }
+
+    private async void OnMapInfo(object? sender, MapInfoEventArgs e)
+    {
+        var mapInfo = e.GetMapInfo(MapControl.Map.Layers);
+        
+        if (mapInfo.Feature?["Location"] is Location location)
+        {
+            var parameters = new Dictionary<string, object>
+            {
+                ["id"] = location.Id.ToString()
+            };
+            await Shell.Current.GoToAsync(nameof(LocationDetailPage), parameters);
+        }
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await _viewModel.GetLocationsAsync();
+    }
+
+    protected override void OnDisappearing()
+    {
+        MapControl.Info -= OnMapInfo;
+        base.OnDisappearing();
     }
 }
--- MemoryTrave.Maui/ViewModel/MapViewModel.cs
+++ MemoryTrave.Maui/ViewModel/MapViewModel.cs
@@ -1,10 +1,17 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Mapsui;
+using Mapsui.Layers;
 using Mapsui.Projections;
+using Mapsui.Styles;
 using Mapsui.Tiling;
+using MemoryTrave.Maui.Infrastructure.Api;
+using MemoryTrave.Maui.Resources.Localization;
 using MemoryTrave.Maui.Services.Interfaces;
 using MemoryTrave.Maui.View;
+using Brush = Mapsui.Styles.Brush;
+using Color = Mapsui.Styles.Color;
+using Location = MemoryTrave.Maui.Models.Location.Location;
 using Map = Mapsui.Map;
 
 namespace MemoryTrave.Maui.ViewModel;
@@ -25,8 +32,16 @@ public partial class MapViewModel : ObservableObject
     
     private readonly INavigationService _navigation;
     private readonly IAuthService _authService;
-    
-    public MapViewModel(INavigationService navigation, IAuthService authService)
+    private readonly IDialogService _dialogService;
+    private readonly ApiRequestService _apiService;
+
+    private MemoryLayer _locationsLayer;
+
+    public MapViewModel(
+        INavigationService navigation, 
+        IAuthService authService,
+        IDialogService dialogService,
+        ApiRequestService apiService)
     {
         Map = new Map();
         Map.Layers.Add(OpenStreetMap.CreateTileLayer());
@@ -41,11 +56,75 @@ public partial class MapViewModel : ObservableObject
         Map.Refresh();
         
         _authService = authService;
+        _dialogService = dialogService;
+        _apiService = apiService;
         _navigation = navigation;
         
         _authService.AuthStateChanged += OnAuthStateChanged;
-        
         UpdateLoginButtonVisibility();
+    }
+
+    public async Task GetLocationsAsync()
+    {
+        var result = await _apiService.GetRequest<List<Location>>(URL.GetLocations());
+        
+        if (!result.IsSuccess && result.ErrorMessage != null)
+        {
+            await _dialogService.ShowMessage(Localization.Error, result.ErrorMessage);
+            return;
+        }
+        
+        if (result.IsSuccess && result.Data != null)
+        {
+            DisplayLocationsOnMap(result.Data);
+        }
+        else
+        {
+            await _dialogService.ShowMessage(Localization.Error, Localization.UnexpectedError);
+        }
+    }
+
+    private void DisplayLocationsOnMap(IEnumerable<Location> locations)
+    {
+        if (_locationsLayer == null)
+        {
+            _locationsLayer = new MemoryLayer
+            {
+                Name = "UserLocations" 
+            };
+            Map.Layers.Add(_locationsLayer);
+        }
+        
+        var features = new List<IFeature>();
+        
+        foreach (var loc in locations)
+        {
+            var mapPoint = SphericalMercator.FromLonLat(loc.Longitude, loc.Latitude);
+            
+            var feature = new Mapsui.Nts.GeometryFeature
+            {
+                Geometry = new NetTopologySuite.Geometries.Point(mapPoint.x, mapPoint.y),
+                ["Id"] = loc.Id
+            };
+            
+            feature.Styles.Add(CreatePinStyle());
+            
+            features.Add(feature);
+        }
+        
+        _locationsLayer.Features = features;
+        
+        Map.Refresh();
+    }
+
+    private SymbolStyle CreatePinStyle()
+    {
+        return new SymbolStyle
+        {
+            SymbolScale = 0.8,
+            Fill = new Brush { Color = Color.FromRgba(66, 133, 244, 255) },
+            Outline = new Pen { Color = Color.White, Width = 2 }
+        };
     }
     
     private void OnAuthStateChanged() => UpdateLoginButtonVisibility();
