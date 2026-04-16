56fe7a85e1fe02f306d861e415d6478b7329ed11
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-04-16 22:39:03 +0300

    up: friends view model #22

--- MemoryTrave.Maui/ViewModel/FriendsViewModel.cs
+++ MemoryTrave.Maui/ViewModel/FriendsViewModel.cs
@@ -2,15 +2,21 @@ using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using MemoryTrave.Maui.Infrastructure.Api;
+using MemoryTrave.Maui.Infrastructure.Security;
+using MemoryTrave.Maui.Models;
+using MemoryTrave.Maui.Models.Articles;
+using MemoryTrave.Maui.Models.Articles.Access;
 using MemoryTrave.Maui.Models.Enums;
 using MemoryTrave.Maui.Models.Friends;
 using MemoryTrave.Maui.Resources.Localization;
 using MemoryTrave.Maui.Services.Dialog;
+using MemoryTrave.Maui.Services.PrivateKey;
 
 namespace MemoryTrave.Maui.ViewModel;
 
 public partial class FriendsViewModel(
     ApiRequestService apiService,
+    IPrivateKeyService privateKeyService,
     IDialogService dialogService) : ObservableObject
 {
     [ObservableProperty]
@@ -36,14 +42,72 @@ public partial class FriendsViewModel(
     [RelayCommand]
     private async Task ConfirmRequestAsync(Guid id)
     {
-        var result = await apiService.PostRequest<bool>(URL.ConfirmRequest(id.ToString()));
+        var result = await apiService.PostRequest<GetId>(URL.ConfirmRequest(id.ToString()));
         if (!result.IsSuccess && result.ErrorMessage != null)
             await dialogService.ShowMessage(Localization.Error, result.ErrorMessage);
-        else if (result.IsSuccess)
+        else if (result.IsSuccess && result.Data != null)
         {
             var deleteObject = Requests.First(f => f.Id == id);
             Requests.Remove(deleteObject);
             Friends.Add(deleteObject);
+
+            var privateArticlesResult = await apiService.GetRequest<List<GetPrivateForFriend>>(URL.GetMyPrivate());
+            if(!privateArticlesResult.IsSuccess && privateArticlesResult.ErrorMessage != null)
+            {
+                await dialogService.ShowMessage(Localization.Error, privateArticlesResult.ErrorMessage);
+                return;
+            }
+            else if (!privateArticlesResult.IsSuccess)
+            {
+                await dialogService.ShowMessage(Localization.Error, Localization.UnexpectedError);
+                return;
+            }
+            
+            var friendId = result.Data.Id;
+            var friendPublicKeyResult = await apiService.GetRequest<GetPublicKey>(URL.GetPublicKeyById(friendId.ToString()));
+            if (!friendPublicKeyResult.IsSuccess && friendPublicKeyResult.ErrorMessage != null)
+            {
+                await dialogService.ShowMessage(Localization.Error, friendPublicKeyResult.ErrorMessage);
+                return;
+            }
+            else if (!friendPublicKeyResult.IsSuccess)
+            {   
+                await dialogService.ShowMessage(Localization.Error, Localization.UnexpectedError);
+                return; 
+            }
+            
+            var privateArticles = privateArticlesResult.Data;
+            
+            var friendPublicKeyString = friendPublicKeyResult.Data.PublicKey;
+            var friendPublicKey = EccP256.StringToPublicKey(friendPublicKeyString);
+            
+            var privateKeyString = privateKeyService.GetKey();
+            var privateKey = EccP256.StringToPrivateKey(privateKeyString);
+
+            var access = new List<AddEncryptedKeysForAddFriend>();
+            foreach (var article in privateArticles)
+            {
+                var encryptedDek = Convert.FromBase64String(article.EncryptedKey);;
+                var dek = EccP256.Decrypt(privateKey, encryptedDek);
+                
+                var encryptedDekForFriend = EccP256.Encrypt(friendPublicKey, dek);
+
+                var newAccess = new AddEncryptedKeysForAddFriend()
+                {
+                    EncryptedKey = Convert.ToBase64String(encryptedDekForFriend),
+                    ArticleId = article.ArticleId
+                };
+                
+                access.Add(newAccess);
+            }
+            
+            var addResponse = await apiService.PostRequest<List<AddEncryptedKeysForAddFriend>, bool>
+                (URL.AddAccessForUser(friendId.ToString()), access);
+            
+            if (!addResponse.IsSuccess &&  addResponse.ErrorMessage != null)
+                await dialogService.ShowMessage(Localization.Error, addResponse.ErrorMessage);
+            else if (!addResponse.IsSuccess)
+                await dialogService.ShowMessage(Localization.Error, Localization.UnexpectedError);
         }
         else
             await dialogService.ShowMessage(Localization.Error, Localization.UnexpectedError);
