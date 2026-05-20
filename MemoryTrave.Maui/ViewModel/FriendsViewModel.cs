a1b1898927006e9ac8a149f8807fb4e9b81f44ec
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-05-21 01:10:32 +0300

    up(friendViewModel): change update data

--- MemoryTrave.Maui/ViewModel/FriendsViewModel.cs
+++ MemoryTrave.Maui/ViewModel/FriendsViewModel.cs
@@ -18,7 +18,6 @@ namespace MemoryTrave.Maui.ViewModel;
 
 public partial class FriendsViewModel(
     ApiRequestService apiService,
-    IPrivateKeyService privateKeyService, 
     INavigationService navigation,
     IDialogService dialogService) : ObservableObject
 {
@@ -50,67 +49,7 @@ public partial class FriendsViewModel(
             await dialogService.ShowMessage(Localization.Error, result.ErrorMessage);
         else if (result.IsSuccess && result.Data != null)
         {
-            var deleteObject = Requests.First(f => f.Id == id);
-            Requests.Remove(deleteObject);
-            Friends.Add(deleteObject);
-
-            var privateArticlesResult = await apiService.GetRequest<List<GetPrivateForFriend>>(URL.GetMyPrivate());
-            if(!privateArticlesResult.IsSuccess && privateArticlesResult.ErrorMessage != null)
-            {
-                await dialogService.ShowMessage(Localization.Error, privateArticlesResult.ErrorMessage);
-                return;
-            }
-            else if (!privateArticlesResult.IsSuccess)
-            {
-                await dialogService.ShowMessage(Localization.Error, Localization.UnexpectedError);
-                return;
-            }
-            
-            var friendId = result.Data.Id;
-            var friendPublicKeyResult = await apiService.GetRequest<GetPublicKey>(URL.GetPublicKeyById(friendId.ToString()));
-            if (!friendPublicKeyResult.IsSuccess && friendPublicKeyResult.ErrorMessage != null)
-            {
-                await dialogService.ShowMessage(Localization.Error, friendPublicKeyResult.ErrorMessage);
-                return;
-            }
-            else if (!friendPublicKeyResult.IsSuccess)
-            {   
-                await dialogService.ShowMessage(Localization.Error, Localization.UnexpectedError);
-                return; 
-            }
-            
-            var privateArticles = privateArticlesResult.Data;
-            
-            var friendPublicKeyString = friendPublicKeyResult.Data.PublicKey;
-            var friendPublicKey = EccP256.StringToPublicKey(friendPublicKeyString);
-            
-            var privateKeyString = privateKeyService.GetKey();
-            var privateKey = EccP256.StringToPrivateKey(privateKeyString);
-
-            var access = new List<AddEncryptedKeysForAddFriend>();
-            foreach (var article in privateArticles)
-            {
-                var encryptedDek = Convert.FromBase64String(article.EncryptedKey);;
-                var dek = EccP256.Decrypt(privateKey, encryptedDek);
-                
-                var encryptedDekForFriend = EccP256.Encrypt(friendPublicKey, dek);
-
-                var newAccess = new AddEncryptedKeysForAddFriend()
-                {
-                    EncryptedKey = Convert.ToBase64String(encryptedDekForFriend),
-                    ArticleId = article.ArticleId
-                };
-                
-                access.Add(newAccess);
-            }
-            
-            var addResponse = await apiService.PostRequest<List<AddEncryptedKeysForAddFriend>, bool>
-                (URL.AddAccessForUser(friendId.ToString()), access);
-            
-            if (!addResponse.IsSuccess &&  addResponse.ErrorMessage != null)
-                await dialogService.ShowMessage(Localization.Error, addResponse.ErrorMessage);
-            else if (!addResponse.IsSuccess)
-                await dialogService.ShowMessage(Localization.Error, Localization.UnexpectedError);
+            await GetFriendsAsync();
         }
         else
             await dialogService.ShowMessage(Localization.Error, Localization.UnexpectedError);
