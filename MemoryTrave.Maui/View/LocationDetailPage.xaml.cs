877ee9ebf21b7f47bc630eb9c1e975ea0252b967
Yauheni Shumsky <136165597+Zhenya035@users.noreply.github.com>
2026-05-26 00:26:18 +0300

    up: add clusterization #4

--- MemoryTrave.Maui/View/LocationDetailPage.xaml.cs
+++ MemoryTrave.Maui/View/LocationDetailPage.xaml.cs
@@ -2,20 +2,35 @@ using MemoryTrave.Maui.ViewModel;
 
 namespace MemoryTrave.Maui.View;
 
+[QueryProperty(nameof(LocationId), "id")]
 public partial class LocationDetailPage : ContentPage
 {
-    private LocationDetailViewModel? _viewModel;
+    private readonly LocationDetailViewModel _viewModel;
+    private string _locationId;
+    public string LocationId
+    {
+        get => _locationId;
+        set
+        {
+            _locationId = value;
+        }
+    }
+
     public LocationDetailPage(LocationDetailViewModel viewModel)
     {
         InitializeComponent();
-        BindingContext = viewModel;
-        
-        _viewModel = viewModel;
+        BindingContext = _viewModel = viewModel;
     }
 
     protected override async void OnAppearing()
     {
         base.OnAppearing();
-        //await _viewModel.GetLocationAsync();
+        
+        if (!string.IsNullOrEmpty(_locationId))
+        {
+            await _viewModel.LoadLocationAsync(_locationId);
+            
+            Title = _viewModel.LocationName;
+        }
     }
 }
